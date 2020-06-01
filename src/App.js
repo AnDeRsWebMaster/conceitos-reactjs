9218b5d37468897e4359505ae9a31aae5bf430d5
Anders <ander.webmaster@uninove.edu.br>
2020-06-01 04:52:43 -0300

    Desafio conceitos do ReactJS GoStack 11

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api"
 import "./styles.css";
 
 function App() {
+  const [repo, setRepo] = useState([])
+
+  useEffect(() => {
+    api.get("repositories")
+    .then(res =>
+      setRepo(res.data)
+    )
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories",{
+      title:"teste1",
+      url:"http://teste.com",
+      techs:"nodejs,reactjs,html,css"
+    })
+    const repositories = response.data
+    setRepo([...repo,repositories])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    const edit = repo.filter(rep => rep.id !== id)
+    setRepo(edit)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repo && repo.map(res =>
+          <li key={res.id}>{res.title}
+            <button onClick={() => handleRemoveRepository(res.id)}>
+              Remover
           </button>
-        </li>
+          </li>)}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>
+        Adicionar</button>
     </div>
   );
 }
